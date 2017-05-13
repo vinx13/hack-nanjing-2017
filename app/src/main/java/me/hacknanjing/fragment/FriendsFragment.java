d507c4063db2ad1110fd711e33924b349cc22cde
Wuwei Lin <vincentl13x@gmail.com>
2017-05-13 23:30:19 +0800

    Friend posts

--- app/src/main/java/me/hacknanjing/fragment/FriendsFragment.java
+++ app/src/main/java/me/hacknanjing/fragment/FriendsFragment.java
@@ -1,24 +1,39 @@
 package me.hacknanjing.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import java.util.List;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import me.hacknanjing.R;
+import me.hacknanjing.activity.CameraActivity;
+import me.hacknanjing.activity.CardAdapter;
+import me.hacknanjing.api.model.Post;
+import me.hacknanjing.util.Factory;
 
 /**
  * Created by Vincent on 2017/5/13.
  */
 
 public class FriendsFragment extends BaseFragment {
-    public static FriendsFragment newInstance() {
+    @BindView(R.id.rv_list)
+    RecyclerView rvList;
+
+
+    public static NowFragment newInstance() {
 
         Bundle args = new Bundle();
 
-        FriendsFragment fragment = new FriendsFragment();
+        NowFragment fragment = new NowFragment();
         fragment.setArguments(args);
         return fragment;
     }
@@ -27,6 +42,19 @@ public class FriendsFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_friends, container, false);
         ButterKnife.bind(this, view);
+
+
+        List<Post> posts = mockPosts();
+        //rvList.setHasFixedSize(true);
+        rvList.setLayoutManager(new LinearLayoutManager(getContext()));
+        rvList.setAdapter(new CardAdapter(getContext(), posts));
+
         return view;
     }
+
+    private List<Post> mockPosts() {
+        return Factory.getInstance().getAllFriendPosts();
+    }
+
 }
+
--- app/src/main/java/me/hacknanjing/util/Factory.java
+++ app/src/main/java/me/hacknanjing/util/Factory.java
@@ -16,6 +16,8 @@ import me.hacknanjing.api.model.User;
 public class Factory {
     ArrayList<User> users;
     ArrayList<Post> posts;
+    ArrayList<Post> friendPosts;
+
     private static Factory instance;
 
     public static Factory getInstance() {
@@ -40,6 +42,14 @@ public class Factory {
         posts.add(new Post(randomUser(), R.drawable.p6, "好耐冇逛深水埗"));
         posts.add(new Post(randomUser(), R.drawable.p7, "海边 等一个人和我牵手"));
         posts.add(new Post(randomUser(), R.drawable.p2, "Quebec！"));
+        friendPosts = new ArrayList<>(6);
+        friendPosts.add(new Post(randomUser(), R.drawable.fp1, "南山南 北海北 南山有墓碑"));
+        friendPosts.add(new Post(randomUser(), R.drawable.fp2, "女人出门果然就是自拍 预祝白白参加中国新歌声成功"));
+        friendPosts.add(new Post(randomUser(), R.drawable.fp3, "斯里兰卡"));
+        friendPosts.add(new Post(randomUser(), R.drawable.fp4, "心里的郁结一下子被风吹走了"));
+        friendPosts.add(new Post(randomUser(), R.drawable.fp5, "永远收不住的放荡不羁"));
+        friendPosts.add(new Post(randomUser(), R.drawable.fp6, "Hey!China!"));
+
     }
 
     public User randomUser() {
@@ -62,6 +72,9 @@ public class Factory {
         return posts;
     }
 
+    public List<Post> getAllFriendPosts() {
+        return friendPosts;
+    }
 
     private static <T> T randomElement(List<T> elements) {
         int index = new Random().nextInt(elements.size());
