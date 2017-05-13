6197672a43db71382d8fb27e05f99b02860a211a
Wuwei Lin <vincentl13x@gmail.com>
2017-05-14 03:48:46 +0800

    Edit

--- app/src/main/java/me/hacknanjing/activity/EditActivity.java
+++ app/src/main/java/me/hacknanjing/activity/EditActivity.java
@@ -1,5 +1,11 @@
 package me.hacknanjing.activity;
 
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
 import me.hacknanjing.R;
 
 /**
@@ -7,6 +13,18 @@ import me.hacknanjing.R;
  */
 
 public class EditActivity extends BaseActivity{
+    final public static String EXTRA_IMAGE = "EXTRA_IMAGE";
+@BindView(R.id.iv_body)
+ImageView ivBody;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String url = getIntent().getStringExtra(EXTRA_IMAGE);
+        if(url!=null) {
+            Picasso.with(this).load(url).into(ivBody);
+        }
+    }
+
     @Override
     protected int getContentViewId() {
         return R.layout.activity_edit;
